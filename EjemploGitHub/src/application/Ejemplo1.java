ca169053b58404c96881d6b6f812ba749c495c34
paula <paula@LAPTOP-A06N6JVJ>
2021-05-25 18:09:20 +0200

    Versión de Paula

--- EjemploGitHub/src/application/Ejemplo1.java
+++ EjemploGitHub/src/application/Ejemplo1.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -27,99 +32,110 @@ public class Ejemplo1 extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		
-		//Creamos un GridPane
+
+		// Creamos un GridPane
 		GridPane grid = new GridPane();
-		
-		//Ponemos un titulo al formulario y le damos formato.
-		Text formulario= new Text("Formulario Mascotas");
+
+		// Ponemos un titulo al formulario y le damos formato.
+		Text formulario = new Text("Formulario Mascotas");
 		formulario.setFill(Color.BLUE);
 		formulario.setFont(Font.font("Calibri", FontWeight.THIN, FontPosture.REGULAR, 40));
-	
+
 		Text nombre = new Text("Nombre:");
-		TextField txtnombre= new TextField();
-		
+		TextField txtnombre = new TextField();
+
 		Text edad = new Text("Edad:");
 		ComboBox<Integer> edad1 = new ComboBox<Integer>();
-		for(int i = 1; i <= 20; i++) {
+		for (int i = 1; i <= 20; i++) {
 			edad1.getItems().add(i);
 		}
-		
+
 		Text raza = new Text("Raza:");
 		ComboBox<String> raza1 = new ComboBox<String>();
 		raza1.getItems().addAll("pastor alemán", "bulldog", "chihuahua", "pitbull", "shih tsu");
-		
-		
-		CheckBox pedigri = new CheckBox ("¿Tiene pedigrí?");
+
+		CheckBox pedigri = new CheckBox("¿Tiene pedigrí?");
 		pedigri.setSelected(true);
-		
-		
+
 		Text precio = new Text("Precio:");
-		//creamos un slider (barrita deslizante)
-		
-        Slider slider= new Slider(1,2000,10);
-        //muestra los numeros debajo del slider.
+		// creamos un slider (barrita deslizante)
+
+		Slider slider = new Slider(1, 2000, 10);
+		// muestra los numeros debajo del slider.
 		slider.setShowTickLabels(true);
-		//Indica si el valor del control deslizante siempre debe estar alineado
-		//con las marcas de graduación. Esto se respeta incluso si no se muestran
-		//las marcas de verificación.
+		// Indica si el valor del control deslizante siempre debe estar alineado
+		// con las marcas de graduación. Esto se respeta incluso si no se muestran
+		// las marcas de verificación.
 		slider.setSnapToTicks(true);
-			
-		//Especifica si la implementación de la máscara debe mostrar marcas de verificación.
+
+		// Especifica si la implementación de la máscara debe mostrar marcas de
+		// verificación.
 		slider.setShowTickMarks(true);
-				
-		//La unidad de distancia entre las principales marcas de graduación. Por ejemplo, si 
-		//el mínimo es 0 y el máximo es 100 y el majorTickUnit es 25, entonces habría 5 marcas
-		//de graduación: una en la posición 0, una en la posición 25, una en la posición 50, 
-		//una en la posición 75 y una última en la posición 100.
+
+		// La unidad de distancia entre las principales marcas de graduación. Por
+		// ejemplo, si
+		// el mínimo es 0 y el máximo es 100 y el majorTickUnit es 25, entonces habría 5
+		// marcas
+		// de graduación: una en la posición 0, una en la posición 25, una en la
+		// posición 50,
+		// una en la posición 75 y una última en la posición 100.
 		slider.setMajorTickUnit(10.0);
-				
-		//El número de tics menores que se colocarán entre dos ticks principales. Este número 
-		//debe ser positivo o cero. Los valores fuera de rango deshabilitarán los ticks menores, 
-		//al igual que un valor de cero.
+
+		// El número de tics menores que se colocarán entre dos ticks principales. Este
+		// número
+		// debe ser positivo o cero. Los valores fuera de rango deshabilitarán los ticks
+		// menores,
+		// al igual que un valor de cero.
 		slider.setMinorTickCount(3);
-		
-		Button boton = new Button("Mostrar");
-		
-		
-		BackgroundImage myBI= new BackgroundImage(new Image("application/perro.jpg"),
-		BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
-		BackgroundSize.DEFAULT);
+
+		Button boton = new Button("Cerrar");
+
+		boton.setOnAction(value -> {
+			stage.close();
+			Desktop enlace = Desktop.getDesktop();
+			try {
+				enlace.browse(new URI("https://www.youtube.com/watch?v=CFPLIaMpGrY"));
+			} catch (IOException | URISyntaxException e) {
+				e.getMessage();
+			}
+		});
+
+		BackgroundImage myBI = new BackgroundImage(new Image("application/perro.jpg"), BackgroundRepeat.REPEAT,
+				BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
 		grid.setBackground(new Background(myBI));
-		
+
 		grid.setAlignment(Pos.TOP_CENTER);
-		GridPane.setMargin(nombre, new Insets(10,10,10,10));
-		GridPane.setMargin(txtnombre, new Insets(10,10,10,10));
-		GridPane.setMargin(edad, new Insets(10,10,10,10));
-		GridPane.setMargin(edad1, new Insets(10,10,10,10));
-		GridPane.setMargin(raza, new Insets(10,10,10,10));
-		GridPane.setMargin(raza1, new Insets(10,10,10,10));
-		GridPane.setMargin(pedigri, new Insets(10,10,10,10));
-		GridPane.setMargin(precio, new Insets(10,10,10,10));
-		GridPane.setMargin(slider, new Insets(10,10,10,10));
-		GridPane.setMargin(boton, new Insets(10,10,10,10));
-		
+		GridPane.setMargin(nombre, new Insets(10, 10, 10, 10));
+		GridPane.setMargin(txtnombre, new Insets(10, 10, 10, 10));
+		GridPane.setMargin(edad, new Insets(10, 10, 10, 10));
+		GridPane.setMargin(edad1, new Insets(10, 10, 10, 10));
+		GridPane.setMargin(raza, new Insets(10, 10, 10, 10));
+		GridPane.setMargin(raza1, new Insets(10, 10, 10, 10));
+		GridPane.setMargin(pedigri, new Insets(10, 10, 10, 10));
+		GridPane.setMargin(precio, new Insets(10, 10, 10, 10));
+		GridPane.setMargin(slider, new Insets(10, 10, 10, 10));
+		GridPane.setMargin(boton, new Insets(10, 10, 10, 10));
+
 		GridPane.setColumnSpan(formulario, 2);
 		GridPane.setColumnSpan(slider, 2);
 		GridPane.setColumnSpan(pedigri, 2);
-		
+
 		grid.add(formulario, 0, 0);
 		grid.add(nombre, 0, 1);
 		grid.add(edad, 0, 2);
 		grid.add(raza, 0, 3);
 		grid.add(pedigri, 0, 4);
 		grid.add(precio, 0, 5);
-		
+
 		grid.add(txtnombre, 1, 1);
 		grid.add(edad1, 1, 2);
 		grid.add(raza1, 1, 3);
 		grid.add(slider, 1, 5);
 		grid.add(boton, 1, 6);
-		
-		
-		Scene scene = new Scene(grid,500.0,330.0);
+
+		Scene scene = new Scene(grid, 500.0, 330.0);
 		stage.setScene(scene);
-		
+
 		stage.show();
 	}
 
